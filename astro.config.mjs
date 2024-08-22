9849ec172c671d124c158b92066ee5adb03b7fa9
Hrihorii Ilin <devopsick@pm.me>
2024-08-22 11:07:50 +0000

    move to local

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
-import mdx from '@astrojs/mdx'
-import AutoImport from 'unplugin-auto-import/astro'
-import { baseLocale, locales } from './src/i18n/config'
-import { remarkReadingTime } from './src/utils/readTime'
-import rehypeExternalLinks from 'rehype-external-links'
+import mdx from '@astrojs/mdx';
+import AutoImport from 'unplugin-auto-import/astro';
+import { baseLocale, locales } from './src/i18n/config';
+import { remarkReadingTime } from './src/utils/readTime';
+import rehypeExternalLinks from 'rehype-external-links';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,67 +22,60 @@ export default defineConfig({
     prefetchAll: true
   },
   site: 'https://fumabun.vercel.app',
-  integrations: [
-    AutoImport({
-        defaultExportByFilename: false,
-        include: [
-            /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
-        ],
-        packagePresets: ['detect-browser-es'],
-        imports: ['react', 'react-router'],
-        dirs: ['./src/utils/*.ts', './src/hooks'],
-        dts: './src/auto-imports.d.ts'
-    }),
-    mdx({
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            theme: 'github-dark-dimmed',
-            wrap: true
-        },
-        gfm: true
-    }),
-    sitemap({
-        entryLimit: 10000,
-        i18n: {
-            filter: (page) => page !== `${SITE}/admin`,
-            defaultLocale: baseLocale,
-            locales: locales
-        }
-    }),
-    robotsTxt({
-        sitemap: 'https://www.fumabun.vercel.app/sitemap-0.xml',
-        host: 'fumabun.vercel.app'
-        }),
-    partytown({
-        config: {
-            forward: ['dataLayer.push'],
-            debug: false
-        }
-    }),
-    react()],
+  integrations: [AutoImport({
+    defaultExportByFilename: false,
+    include: [/\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+    ],
+    packagePresets: ['detect-browser-es'],
+    imports: ['react', 'react-router'],
+    dirs: ['./src/utils/*.ts', './src/hooks'],
+    dts: './src/auto-imports.d.ts'
+  }), mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      wrap: true
+    },
+    gfm: true
+  }), sitemap({
+    entryLimit: 10000,
     i18n: {
-		defaultLocale: baseLocale,
-		locales: locales,
-		routing: {
-			prefixDefaultLocale: true
-		}
-	},
-	markdown: {
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					content: { type: 'text', value: ' 🔗' },
-					target: '_blank',
-					rel: ['nofollow', 'noreferrer']
-				}
-			]
-		],
-		remarkPlugins: [remarkReadingTime],
-		shikiConfig: {
-			theme: 'github-dark-dimmed',
-			wrap: true
-		},
-		gfm: true
-	}
+      filter: page => page !== `${SITE}/admin`,
+      defaultLocale: baseLocale,
+      locales: locales
+    }
+  }), robotsTxt({
+    sitemap: 'https://www.fumabun.vercel.app/sitemap-0.xml',
+    host: 'fumabun.vercel.app'
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+      debug: false
+    }
+  }), react()],
+  i18n: {
+    defaultLocale: baseLocale,
+    locales: locales,
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      },
+      target: '_blank',
+      rel: ['nofollow', 'noreferrer']
+    }]],
+    remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      wrap: true
+    },
+    gfm: true
+  },
+  output: "server",
+  adapter: vercel()
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="astro/astro-jsx" />
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
+/// <reference types="astro/astro-jsx" />
 
+type MyAttributes = astroHTML.JSX.ImgHTMLAttributes;
